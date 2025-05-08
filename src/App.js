9a9bce02725acd06ea264166e709bf1b2c0d6080
Aryan49Choudhary <choudharyaryan120203@gmail.com>
2025-05-08 14:31:05 +0000

    first devops project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 function App() {
   return (
     <div style={{ textAlign: 'center', paddingTop: '50px' }}>
-      <h1>Hello! I am Aryan Choudhary</h1>
+      <h1>Hello! I am Aryan Choudhary.</h1>
       <p>This is a React app deployed using a full DevOps pipeline using jenkins, ansible and docker 🚀</p>
-      <p>And it went well.</p>
+      <p>It's my first devops project and it went well.</p>
     </div>
   );
 }
