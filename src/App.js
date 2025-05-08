339468dea5859e39141126574866534cbafe4d37
Aryan49Choudhary <choudharyaryan120203@gmail.com>
2025-05-08 13:59:25 +0000

    intro updated

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ function App() {
   return (
     <div style={{ textAlign: 'center', paddingTop: '50px' }}>
       <h1>Hello! I am Aryan Choudhary</h1>
-      <p>This is a React app deployed using a full DevOps pipeline 🚀</p>
+      <p>This is a React app deployed using a full DevOps pipeline using jenkins, ansible and docker 🚀</p>
     </div>
   );
 }
